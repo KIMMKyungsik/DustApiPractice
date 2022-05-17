479bb520577a96b9bdae5224b0ed5fb9ff748354
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-05-17 12:03:01 +0900

    미세먼지 API 연습

---
+++ app/src/main/java/com/application/dustapipractice/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.application.dustapipractice
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        requestLocationPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (!locationPermissionGranted){
+            finish()
+
+        }
+        else{
+            //fetch Data
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+
+            ), 100
+
+        )
+
+
+    }
+
+    companion object {
+        const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
+    }
+}
