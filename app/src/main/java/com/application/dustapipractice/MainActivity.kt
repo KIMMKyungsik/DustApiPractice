04dd847eefe9076d6333bf1835d2beaace5315e5
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-05-17 13:51:25 +0900

    경 위도 위치정보를 가져와 텍스트뷰에 띄워주기

--- app/src/main/java/com/application/dustapipractice/MainActivity.kt
+++ app/src/main/java/com/application/dustapipractice/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.application.dustapipractice
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityManagerCompat
+import com.application.dustapipractice.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? = null
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-
+        initVariables()
         requestLocationPermissions()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
+    }
+
+    @SuppressLint("MissingPermission")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -26,13 +49,16 @@ class MainActivity : AppCompatActivity() {
             requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED
 
-        if (!locationPermissionGranted){
+        if (!locationPermissionGranted) {
             finish()
+        } else {
+            fetchAirQualityData()
+        }
+    }
 
-        }
-        else{
-            //fetch Data
-        }
+    private fun initVariables() {
+        // 마지막으로 확인된 위치 정보 얻기
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     private fun requestLocationPermissions() {
@@ -41,15 +67,26 @@ class MainActivity : AppCompatActivity() {
             arrayOf(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION
-
-            ), 100
-
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
         )
+    }
 
+    @SuppressLint("MissingPermission")
+    private fun fetchAirQualityData() {
+        // fetchData
+        cancellationTokenSource = CancellationTokenSource()
 
+        fusedLocationProviderClient.getCurrentLocation(
+            LocationRequest.PRIORITY_HIGH_ACCURACY,
+            cancellationTokenSource!!.token
+        ).addOnSuccessListener { location ->
+
+            binding.textView.text = "${location.latitude},${location.longitude}"
+        }
     }
 
     companion object {
-        const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
