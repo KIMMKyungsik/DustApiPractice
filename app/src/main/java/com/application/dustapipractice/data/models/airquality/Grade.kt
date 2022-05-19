0c2bc379486963d7160f0e54a6650b1e185faaed
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-05-19 19:25:32 +0900

    위젯을 만들기 위한 xml 만들기

--- app/src/main/java/com/application/dustapipractice/data/models/airquality/Grade.kt
+++ app/src/main/java/com/application/dustapipractice/data/models/airquality/Grade.kt
@@ -4,22 +4,24 @@ import androidx.annotation.ColorRes
 import com.application.dustapipractice.R
 import com.google.gson.annotations.SerializedName
 
-enum class Grade(val label: String,
-                 val emoji: String,
-               @ColorRes  val colorResID: Int) {
+enum class Grade(
+    val label: String,
+    val emoji: String,
+    @ColorRes val colorResID: Int
+) {
     @SerializedName("1")
-    GOOD("좋음","\uD83D\uDE06", R.color.blue),
+    GOOD("좋음", "\uD83D\uDE06", R.color.blue),
 
     @SerializedName("2")
-    NOMAL("보통","\uD83D\uDE42",R.color.green),
+    NOMAL("보통", "\uD83D\uDE42", R.color.green),
 
     @SerializedName("3")
-    BAD("나쁨","\uD83D\uDE1E",R.color.yellow),
+    BAD("나쁨", "\uD83D\uDE1E", R.color.yellow),
 
     @SerializedName("4")
-    AWFUL("매우나쁨","\uD83E\uDD2E",R.color.red),
+    AWFUL("매우나쁨", "\uD83E\uDD2E", R.color.red),
 
-    UNKNOWN("미측정","\uD83E\uDD25",R.color.gray);
+    UNKNOWN("미측정", "\uD83E\uDD25", R.color.gray);
 
     override fun toString(): String {
         return "$label $emoji"
