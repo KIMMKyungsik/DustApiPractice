2ca8fc14ce1402e4f1eb7ad99de96f096c0bf362
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-05-19 19:38:40 +0900

    위젯관련 매니페스트 코드 추가

---
+++ app/src/main/java/com/application/dustapipractice/appwidget/SimpleAirQualityWidgetProvider.kt
@@ -0,0 +1,16 @@
+package com.application.dustapipractice.appwidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class SimpleAirQualityWidgetProvider:AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
